21c6fcb0440ad24ccd8c4753bf466cae9b6f5a27
USER <user@USERs-MacBook-Pro.local>
2020-03-05 09:40:42 -0500

    Updates

---
+++ precache-manifest.278c871aa7630d049074d6cf5bf88495.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d348904fe5c4565f19bbee129c56d59",
+    "url": "/Portfolio/static/media/background.2d348904.jpg"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "cdbd3ab43f4d78577772",
+    "url": "/Portfolio/static/js/main.cdbd3ab4.chunk.js"
+  },
+  {
+    "revision": "bd8f3a1e0fcd43356c52",
+    "url": "/Portfolio/static/js/1.bd8f3a1e.chunk.js"
+  },
+  {
+    "revision": "cdbd3ab43f4d78577772",
+    "url": "/Portfolio/static/css/main.d68ac50f.chunk.css"
+  },
+  {
+    "revision": "fc1a2658bbdf72a920e671a8a8700b35",
+    "url": "/Portfolio/index.html"
+  }
+];
