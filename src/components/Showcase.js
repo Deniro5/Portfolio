ff08e84289a8866aabe6399b4f63c2a1dcc05b7e
USER <user@USERs-MacBook-Pro.local>
2020-03-07 18:36:32 -0500

    added project

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -9,7 +9,7 @@ const projects = [
   {src: "https://github.com/Deniro5/Clinic" ,link: "https://deniro5.github.io/Clinic/", imgsrc: "imgs/clinic.png", title: "Clinic Template", Description: "Clinic Website Template. View information about the clinic and it's doctors. Contact the clinic about any inquiries."},
   {src: "https://github.com/Deniro5/Notes" ,link: "https://deniro5.github.io/Notes/", imgsrc: "imgs/notes.png", title: "Notes", Description: "Notes App. Create, Edit and delete notes. Search for notes that you have created. Notes are stored in local storage."},
   {src: "https://github.com/Deniro5/Countries" ,link: "https://deniro5.github.io/Countries/", imgsrc: "imgs/countries.png", title: "Country Info Website", Description: "Country information site. Displays information such as population, size and currency about all countries around the world. Made with the help of the restcountries API."},
-//  {imgsrc: "imgs/pokedex.png", title: "Pokedex", Description: "Pokedex App. Displays basic information about 800+ pokemon. Can search for specific pokemon using the search bar or navigate using the arrows. Made with the help of pokeapi API."},
+  {src: "https://github.com/Deniro5/Pokedex", link: "https://deniro5.github.io/Pokedex/", imgsrc: "imgs/pokedex.png", title: "Pokedex", Description: "Pokedex App. Displays basic information about 800+ pokemon. Can search for specific pokemon using the search bar or navigate using the arrows. Made with the help of pokeapi API."},
   {src: "https://github.com/Deniro5/Gram", link: "https://rocky-lake-89848.herokuapp.com/" , imgsrc: "imgs/gram.png", title: "Instagram Remake", Description: "Mock social media website. Sign in as a@a.com, b@b.com .... e@e.com. Password is first letter of username, Made using React and Node.js"},
 ]
 
